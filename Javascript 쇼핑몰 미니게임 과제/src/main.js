1a5ca3bb3a227a15db1d91bb4c3aabcfa3f1ac47
taeyeonyoo <taeyeonyoo16@gmail.com>
2024-07-03 18:27:43 +0900

    JavaScript - handling events

--- Javascript 쇼핑몰 미니게임 과제/src/main.js
+++ Javascript 쇼핑몰 미니게임 과제/src/main.js
@@ -1,7 +1,12 @@
 // Fetch the items from the JSON file
 function loadItems() {
     return fetch('data/data.json')
-        .then(response => response.json())
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok ' + response.statusText);
+            }
+            return response.json();
+        })
         .then(json => json.items);
 }
 
@@ -17,6 +22,24 @@ function createHTMLString(item) {
             <span class="item__description">${item.gender}, ${item.size}</span>
         </li>
     `;
+}
+
+function onButtonClick(event, items) {
+    const dataset = event.target.dataset;
+    const key = dataset.key;
+    const value = dataset.value; 
+    if (key == null || value == null) {
+        return;
+    }
+
+    displayItems(items.filter(item => item[key] == value));
+}
+
+function setEventListeners(items) {
+    const logo = document.querySelector('.logo');
+    const buttons = document.querySelector('.buttons');
+    logo.addEventListener('click', () => displayItems(items));
+    buttons.addEventListener('click', event => onButtonClick(event, items));
 }
 
 // main
