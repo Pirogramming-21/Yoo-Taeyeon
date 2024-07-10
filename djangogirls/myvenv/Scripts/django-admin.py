07d4d2ef9ec944580042e64377bbbdb16e0618aa
taeyeonyoo <taeyeonyoo16@gmail.com>
2024-07-11 01:36:17 +0900

    Django 설치하기

---
+++ djangogirls/myvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\taeye\piro21\Yoo-Taeyeon\djangogirls\myvenv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
